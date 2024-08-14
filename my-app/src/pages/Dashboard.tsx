a47372cce17bb7b4a8810c555d1e2c6fd1fe24e5
Arthur Cunha <127213303+Arthucunha@users.noreply.github.com>
2024-08-14 19:54:49 -0300

    Adding some style to dashboard

--- my-app/src/pages/Dashboard.tsx
+++ my-app/src/pages/Dashboard.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+// import React from 'react'
+
+// const Dashboard = () => {
+//     return (
+//         <div>Dashboard</div>
+//     )
+// }
+
+// export default Dashboard
+
+import React from 'react';
 
 const Dashboard = () => {
-    return (
-        <div>Dashboard</div>
-    )
-}
+  return (
+    <div className="flex flex-col items-center justify-center h-screen"> 
+      {/* O contêiner acima centraliza o conteúdo vertical e horizontalmente */}
+      <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-gray-800"> 
+        {/* Texo grande e responsivo */}
+        Bem-Vindo!
+      </h1>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
