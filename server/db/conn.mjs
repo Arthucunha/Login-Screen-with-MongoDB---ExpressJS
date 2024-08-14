fbc0a7fe79c6520ea68a02cc3575c8d3917e9cfa
Arthur Cunha <127213303+Arthucunha@users.noreply.github.com>
2024-08-14 18:59:03 -0300

    fixing error on database name

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,11 +1,14 @@
 import { MongoClient } from "mongodb";
-const connectionString = process.env.ATLAS_URI || "";
+const connectionString = process.env.URI || "";
 const client = new MongoClient(connectionString);
 let conn;
 try {
+    console.log("chegouaqui1")
   conn = await client.connect();
+  console.log("chegouaqui2")
+
 } catch(e) {
   console.error(e);
 }
-let db = conn.db("sample_training");
+let db = conn.db("loginApp")
 export default db;
