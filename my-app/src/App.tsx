6944074d5dba768b0a61f855844386a1085ee9d7
Arthur Cunha <127213303+Arthucunha@users.noreply.github.com>
2024-08-14 04:17:00 -0300

    Creating the form box and form title

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,21 +4,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div className='min-h-screen bg-gray-50 flex flex-col justify-center'>
+        <div className='max-w-md w-full mx-auto mt-4 bg-white p-8 border border-gray-300'>
+            <div className='text-3xl font-bold text-gray-900 mt-2 text-center'>Entrar</div>
+        </div>
     </div>
   );
 }
