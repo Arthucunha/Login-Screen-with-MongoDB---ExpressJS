5be81dd9d47e47208fbd14adaf1600ff0896874e
Arthur Cunha <127213303+Arthucunha@users.noreply.github.com>
2024-08-14 21:25:21 -0300

    Removing some useless console.logs

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -3,9 +3,8 @@ const connectionString = process.env.URI || "";
 const client = new MongoClient(connectionString);
 let conn;
 try {
-    console.log("chegouaqui1")
+  
   conn = await client.connect();
-  console.log("chegouaqui2")
 
 } catch(e) {
   console.error(e);
